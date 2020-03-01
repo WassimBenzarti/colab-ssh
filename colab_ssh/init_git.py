af81126568f27c345858cf73b47924d593d30477
Wassim Benzarti <wass11121996@gmail.com>
2020-03-01 15:13:12 +0000

    fix the issue of private repo

--- colab_ssh/init_git.py
+++ colab_ssh/init_git.py
@@ -1,11 +1,14 @@
 from colab_ssh._command import run_command as _run_command
 import os
 
-def init_git(repositoryUrl, branch="master"):
+def init_git(repositoryUrl, branch="master", github_personal_token=""):
     _run_command("git clone {}".format(repositoryUrl))
     _run_command("git checkout {}".format(branch))
 
     # TODO: Create the folder if it doesn't exist
-    os.system('mkdir -p ~/.ssh && curl -L -f {}/raw/{}/.colab_ssh/authorized_keys >> ~/.ssh/authorized_keys'.format(repositoryUrl.rstrip(".git"), branch))
+    os.system("mkdir -p ~/.ssh && curl -L -f {} {}/{}/.colab_ssh/authorized_keys >> ~/.ssh/authorized_keys".format(
+        ("-H 'Authorization: token "+github_personal_token) if github_personal_token else "",
+        repositoryUrl.rstrip(".git").replace("github.com", "raw.githubusercontent.com"), 
+        branch))
 
     print("Successfully cloned the repository")
