a5d21984a9002ff8faf823d02f193dd5ca3ad87f
Wassim Benzarti <wass11121996@gmail.com>
2020-03-01 15:54:32 +0000

    Private repo clone fix

--- colab_ssh/init_git.py
+++ colab_ssh/init_git.py
@@ -2,7 +2,9 @@ from colab_ssh._command import run_command as _run_command
 import os
 
 def init_git(repositoryUrl, branch="master", personal_token=""):
-    _run_command("git clone {}".format(repositoryUrl))
+    _run_command("git clone {}".format(
+            repositoryUrl.replace("github.com", personal_token+"@github.com")) if personal_token else repositoryUrl
+        )
     _run_command("git checkout {}".format(branch))
 
     os.system("mkdir -p ~/.ssh && curl -L -f {} {}/{}/.colab_ssh/authorized_keys >> ~/.ssh/authorized_keys".format(
