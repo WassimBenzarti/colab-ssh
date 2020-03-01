55776bc0c39ee231e20fb25b905205e6e98c4d7e
Wassim Benzarti <wass11121996@gmail.com>
2020-03-01 15:35:27 +0000

    Renamed to personal_token

--- colab_ssh/init_git.py
+++ colab_ssh/init_git.py
@@ -1,13 +1,13 @@
 from colab_ssh._command import run_command as _run_command
 import os
 
-def init_git(repositoryUrl, branch="master", github_personal_token=""):
+def init_git(repositoryUrl, branch="master", personal_token=""):
     _run_command("git clone {}".format(repositoryUrl))
     _run_command("git checkout {}".format(branch))
 
     # TODO: Create the folder if it doesn't exist
     os.system("mkdir -p ~/.ssh && curl -L -f {} {}/{}/.colab_ssh/authorized_keys >> ~/.ssh/authorized_keys".format(
-        ("-H 'Authorization: token {}'".format(github_personal_token)) if github_personal_token else "",
+        ("-H 'Authorization: token {}'".format(personal_token)) if personal_token else "",
         repositoryUrl.rstrip(".git").replace("github.com", "raw.githubusercontent.com"), 
         branch))
 
