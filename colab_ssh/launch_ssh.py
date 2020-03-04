64ad99e7b382f5d3d60f25ecb5b5fe7b63b3b6b9
Wassim Benzarti <wass11121996@gmail.com>
2020-03-04 10:06:34 +0000

    Added a return statement to return the info

--- colab_ssh/launch_ssh.py
+++ colab_ssh/launch_ssh.py
@@ -32,5 +32,7 @@ def launch_ssh(token, password=""):
   info = run_with_pipe('''curl http://localhost:4040/api/tunnels | python3 -c "import sys, json; print(json.load(sys.stdin)['tunnels'][0]['public_url'])"''')
   
   print("Successfully running", info)
+
+  return info
   
 
