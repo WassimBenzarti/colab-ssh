31e7ac647b1c4c693ce2bdf7dab3b65e1e6dd39f
Wassim Benzarti <wass11121996@gmail.com>
2020-05-02 05:10:51 +0100

    Fix the exchange error

--- colab_ssh/launch_direct_ssh.py
+++ colab_ssh/launch_direct_ssh.py
@@ -1,40 +1,52 @@
 import os
+from subprocess import run, PIPE
 
-
-
+print("hello")
 def launch_direct_ssh(username, ip_address, port, 
-		reverse_ports=["6022:127.0.0.1:22"], 
-		public_key_path="%USERPROFILE%/.ssh/id_rsa.pub"):
-	"""
-	Launches a direct ssh connection
-	! IMPORTANT ! This requires that you install openssh-server locally on your machine and
-	open your SSH port (and make sure it's publically available on the internet)
-	Arguments
-		username: The username that you want to connect with (your computer's username)
-		ip_address: The ip address to your machine
-		port: The port where openssh-server is listening
-		reverse_ports: This is used when you want to forward a port to your machine.
-			Use this format <remoteport>:127.0.0.1:<localport>
-		public_key_path: Public key path (Please change it to '~/.ssh/id_rsa.pub' if you are on linux)
-	"""
-	# Kill all ssh intances including the server
-	os.system("pkill -f ssh")
+    reverse_ports=["6022:127.0.0.1:22"], 
+    public_key_path="%USERPROFILE%/.ssh/id_rsa.pub",
+    verbose=1):
+  """
+  Launches a direct ssh connection
+  ! IMPORTANT ! This requires that you install openssh-server locally on your machine and
+  open your SSH port (and make sure it's publically available on the internet)
+  Arguments
+    username: The username that you want to connect with (your computer's username)
+    ip_address: The ip address to your machine
+    port: The port where openssh-server is listening
+    reverse_ports: This is used when you want to forward a port to your machine.
+      Use this format <remoteport>:127.0.0.1:<localport>
+    public_key_path: Public key path (Please change it to '~/.ssh/id_rsa.pub' if you are on linux)
+  """
+  def run_command(command, verbose=True):
+    if verbose:
+      print("Running:", command)
+    process=run(command, stdout=PIPE, stderr=PIPE, shell=True)
+    if verbose:
+      print(process.stdout) if process.stdout else ""
+      print(process.stderr) if process.stderr else ""
 
-	# Download autossh
-	os.system("apt-get install -y autossh openssh-server")
+  # Kill all ssh intances including the server
+  run_command("pkill -f ssh")
 
-	# Get the public key from the client (the one with the ip_address)
-	os.system(f"scp -P {port} {username}@{ip_address}:{public_key_path} ~/.ssh/authorized_keys")
+  # Download autossh
+  run_command("apt-get install -y autossh openssh-server")
 
-	# Permit the root login
-	os.system("echo 'PermitRootLogin yes' >> /etc/ssh/sshd_config")
+  # Permit the root login
+  run_command("echo 'PermitRootLogin yes' >> /etc/ssh/sshd_config")
 
-	# Launch the SSH Server
-	os.system('/usr/sbin/sshd -D &')
+  # Launch the SSH Server
+  run_command('service ssh start')
 
-	# Launch the connection
-	# Prepare the ports
-	reverse_tunnel_ports = " -R ".join(reverse_ports)
-	if len(reverse_ports) >0:
-		reverse_tunnel_ports = " -R "+reverse_tunnel_ports
-	os.system(f'autossh -o "ServerAliveInterval=1" -o "StrictHostKeyChecking=no" -f -T -N {reverse_tunnel_ports} {username}@{ip_address} -p {port}')
+  # Get the public key from the client (the one with the ip_address)
+  run_command(f"scp -P {port} {username}@{ip_address}:{public_key_path} ~/.ssh/authorized_keys")
+
+  # Launch the connection
+  # Prepare the ports
+  reverse_tunnel_ports = " -R ".join(reverse_ports)
+  if len(reverse_ports) >0:
+    reverse_tunnel_ports = " -R "+reverse_tunnel_ports
+  verbosity=""
+  if verbose:
+    verbosity=" -vv "
+  run_command(f'autossh {verbosity} -o "StrictHostKeyChecking=no" -f -T -N {reverse_tunnel_ports} {username}@{ip_address} -p {port}')
