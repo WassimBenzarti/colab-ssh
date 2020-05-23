7024e8c43e08f6516cda62febfa6a592b165ceef
Wassim Benzarti <wass11121996@gmail.com>
2020-05-23 17:13:28 +0000

    Release patch

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="colab_ssh",
-    version="0.2.52",
+    version="0.2.53",
     author="Wassim Benzarti",
     author_email="m.wassim.benzarti@gmail.com",
     description="Google colab SSH connector",
